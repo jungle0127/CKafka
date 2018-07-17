6281bdfd3cc44cb2c7c670f03ad45af8c97554da
zhang.jinghe <zhang.jinghe05016@westone.com.cn>
2018-07-17 09:47:43 +0800

    updatedlogic for consumer

--- consumer.c
+++ consumer.c
@@ -9,10 +9,59 @@
 
 static rd_kafka_t *rk;
 static rd_kafka_topic_partition_list_t *topics;
+static void msg_consume (rd_kafka_message_t *rkmessage,
+       void *opaque) {
+  if (rkmessage->err) {
+    if (rkmessage->err == RD_KAFKA_RESP_ERR__PARTITION_EOF) {
+      fprintf(stderr,
+        "%% Consumer reached end of %s [%"PRId32"] "
+             "message queue at offset %"PRId64"\n",
+             rd_kafka_topic_name(rkmessage->rkt),
+             rkmessage->partition, rkmessage->offset);
+ 
+      return;
+    }
+ 
+    if (rkmessage->rkt)
+            fprintf(stderr, "%% Consume error for "
+                    "topic \"%s\" [%"PRId32"] "
+                    "offset %"PRId64": %s\n",
+                    rd_kafka_topic_name(rkmessage->rkt),
+                    rkmessage->partition,
+                    rkmessage->offset,
+                    rd_kafka_message_errstr(rkmessage));
+    else
+            fprintf(stderr, "%% Consumer error: %s: %s\n",
+                    rd_kafka_err2str(rkmessage->err),
+                    rd_kafka_message_errstr(rkmessage));
+ 
+    if (rkmessage->err == RD_KAFKA_RESP_ERR__UNKNOWN_PARTITION ||
+        rkmessage->err == RD_KAFKA_RESP_ERR__UNKNOWN_TOPIC)
+          run = 0;
+    return;
+  }
+ 
+  fprintf(stdout, "%% Message (topic %s [%"PRId32"], "
+                      "offset %"PRId64", %zd bytes):\n",
+                      rd_kafka_topic_name(rkmessage->rkt),
+                      rkmessage->partition,
+    rkmessage->offset, rkmessage->len);
+ 
+  if (rkmessage->key_len) {
+    printf("Key: %.*s\n",
+             (int)rkmessage->key_len, (char *)rkmessage->key);
+  }
+ 
+  printf("%.*s\n",
+           (int)rkmessage->len, (char *)rkmessage->payload);
+  
+}
 int main(){
 	char *brokers = "192.168.17.199:9092";
 	rd_kafka_conf_t *conf;
 	rd_kafka_topic_conf_t *topic_conf;
+	rd_kafka_resp_err_t err;
+	char errstr[512];
 	// 创建kafka配置
 	conf = rd_kafka_conf_new();
 	// 创建topic配置
@@ -32,7 +81,10 @@ int main(){
 	rd_kafka_subscribe(rk, topics);
 	// 轮询消息或事件，并调用回调函数
 	// TODO: 无限轮询
-	rd_kafka_message_t *rkmessage;
-	rkmessage = rd_kafka_consumer_poll(rk, 1000)
+	while(true){
+		rd_kafka_message_t *rkmessage;
+		rkmessage = rd_kafka_consumer_poll(rk, 1000);
+		msg_consume(rkmessage);
+	}
 
 }
